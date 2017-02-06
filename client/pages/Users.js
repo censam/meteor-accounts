cb12e40167ea5c4065f980dc5979d817e41c1b89
censam <wgsampath@gmail.com>
2017-02-06 10:39:45 +0530

    NPM Packages and Modifying Roles

--- client/pages/Users.js
+++ client/pages/Users.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 Template.Users.onCreated(function(){
 	this.autorun(() => {
 		this.subscribe('allUsers');
@@ -12,6 +14,28 @@ Template.Users.helpers({
 		return this.emails[0].address;
 	},
 	isAdmin : function(){
-		return Roles.userIsInRole(this._id, 'admin') ? 'Admin' : 'Normal';
+		return Roles.userIsInRole(this._id, 'admin') ? 'Admin' : '';
+	},
+	dateFormat : function(){
+		return moment(this.createdAt).format('YYYY - DD - MMMM'); 
+	},
+	editMode : function(){
+		return Session.get('clicked_user') ?  'edit-mode' : '';
+	},
+	currentEdit : function(){
+		let user = Session.get('clicked_user');
+		return user._id === this._id;
+	}
+ });
+
+Template.Users.events({
+	'click .user_id':function(){
+		Session.set('clicked_user', this);
+	},
+	'click .toggle-admin':function(){
+		 Meteor.call('toggleAdmin',this._id);
+	},
+	'click .close-edit-mode' : function(){
+		Session.set('clicked_user', '');
 	}
 });
---
+++ server/methods.js
@@ -0,0 +1,10 @@
+Meteor.methods({
+	toggleAdmin(id){
+		if(Roles.userIsInRole(id, 'admin')){
+		 	Roles.removeUsersFromRoles(id,'admin');
+		 }else{
+		 	Roles.addUsersToRoles(id,'admin');
+
+		 }
+	}
+});
